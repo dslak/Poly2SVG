3f1e1304e62f95d5828f3cb8fec9a1d552eea91a
dslak <dslaky@gmail.com>
2016-12-19 11:25:54 +0100

    Script to download POLY files

---
+++ get_poly.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+minlevel=4
+maxlevel=8
+
+if [ ! -e "apikey" ]
+then
+  echo "Please, fill apikey file with your OSM API key"
+  echo "If file don't exists create it"
+  exit
+fi
+
+
+apikey=$(cat "apikey")   
+
+
+if [ -z "$1" ] && [ -z "$2" ]
+then
+
+    echo "Usage: ./get_poly.sh zipname country_code [level_from level_to]"
+    exit
+
+fi
+
+
+if [ ! -z "$3" ] && [ ! -z "$4" ]
+then
+
+    minlevel=$3
+    maxlevel=$4
+fi
+
+#mkdir tmp
+#cd tmp
+
+curl -f -o $1.zip --url "https://osm.wno-edv-service.de/boundaries/exportBoundaries?apiversion=1.0&apikey=$apikey&exportFormat=poly&exportLayout=split&exportAreas=land&from_al=$minlevel&to_al=$maxlevel&union=false&selected=$2"
+
+echo "DONE!"
